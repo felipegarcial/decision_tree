8dbfe963d226743faa28f7bc84059f8b8234f416
Felipe García <felipegarcial64@gmail.com>
2020-11-07 18:48:13 -0500

    Modifications

--- app/main.py
+++ app/main.py
@@ -9,6 +9,3 @@ def helloWorld():
 @app.route('/desitionTree')
 def getDecisionTree():
     return jsonify({'message':'Image desition tree'})
-
-if __name__ == '__main__':
-    app.run(debug=True,port=4000)
