79c433047e0c0ba6010761d7255acf0714cd0ff9
Felipe García <felipegarcial64@gmail.com>
2020-11-07 17:55:58 -0500

    Added some get methos to create api rest

--- app.py
+++ app.py
@@ -3,5 +3,12 @@ from flask import Flask,jsonify
 
 app = Flask(__name__)
 
+@app.route('/')
+def helloWorld():
+    return jsonify({'message':'Hello world!'})
+@app.route('/decisionTree')
+def getDecisionTree():
+    return jsonify({'message':'Image decision tree'})
+
 if __name__ == '__main__':
     app.run(debug=True,port=4000)
