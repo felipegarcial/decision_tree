98b71c5050b3529150085ba21402f16666bdf5dd
Felipe García <felipegarcial64@gmail.com>
2020-11-13 03:27:36 -0500

    Solving deployed

--- model/tree.py
+++ model/tree.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
 from sklearn import tree
